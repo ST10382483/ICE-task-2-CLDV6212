1b67247a80eb0c50e1540ee812d88c83d397d49b
ST10382483 <129285556+ST10382483@users.noreply.github.com>
2025-09-30 23:00:20 +0200

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10382483_Jordan_Reddy_ICE_task_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
